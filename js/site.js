0c9f02d1654778a830b88ce5c76b28422d2ac25c
seye007 <dronesheye@gmail.com>
2023-06-21 16:51:20 +0100

    initial

---
+++ js/site.js
@@ -0,0 +1,14 @@
+﻿var selectContainer = document.querySelector(".custom-select-container");
+var selectElement = selectContainer.querySelector("select");
+
+selectContainer.addEventListener("click", function () {
+  selectElement.focus();
+});
+
+selectElement.addEventListener("focus", function () {
+  selectContainer.classList.add("show-dropdown");
+});
+
+selectElement.addEventListener("blur", function () {
+  selectContainer.classList.remove("show-dropdown");
+});
